e12d39edb9a281bccbbd8252a5cf1d516327e1c9
Ted Blizzard <me@tedblizzard.com>
2018-02-21 07:30:47 -0500

    upload
    first load

---
+++ main.js
@@ -0,0 +1,18 @@
+/*
+note
+*/
+var myname="Ted Blizzard";
+// document.write("Hello !");
+var red = [0,100,62];
+var orange = [40,100,60];
+var green = [75,100,40];
+var blue = [196,77,55];
+var purple = [280,50,60]
+
+var color=[red, orange,green,blue,purple]
+
+drawName(myname,color);
+
+bounceBubbles();
+
+// bubbleShape
